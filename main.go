b4fb13ef548b773ce258d3fb9c0d78ae5ecb1d7a
Nathaniel Morihara <nathanielmorihara@gmail.com>
2019-05-18 20:57:00 -0400

    Player Movement & Refactoring

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	// "fmt"
 	"bytes"
 	"image"
 	_ "image/png"
@@ -22,39 +21,96 @@ const (
 	frameWidth  = 32
 	frameHeight = 32
 	frameNum    = 8
+
+	playerSpeed    = 1
+	playerAnimRate = 5
 )
 
 var (
-	count       = 0
+	clock       = 0
 	runnerImage *ebiten.Image
-	xPos = float64(0)
+	player      = new(Player)
 )
 
-// Used Animation and Keyboard Tutorials
+type Player struct {
+	PosX         float64
+	PosY         float64
+	VelX         float64
+	VelY         float64
+	CurrentFrame int
+}
 
-func update(screen *ebiten.Image) error {
-	count++
+func init() {
+	img, _, err := image.Decode(bytes.NewReader(images.Runner_png))
+	if err != nil {
+		log.Fatal(err)
+	}
+	runnerImage, _ = ebiten.NewImageFromImage(img, ebiten.FilterDefault)
+}
 
-	if ebiten.IsDrawingSkipped() {
-		return nil
+func (p *Player) Update(screen *ebiten.Image) {
+	if ebiten.IsKeyPressed(ebiten.KeyRight) {
+		p.VelX = playerSpeed
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyLeft) {
+		p.VelX = -playerSpeed
+	}
+	if !ebiten.IsKeyPressed(ebiten.KeyRight) && !ebiten.IsKeyPressed(ebiten.KeyLeft) {
+		p.VelX = 0
 	}
 
-	// drawImageOptions := &ebiten.DrawImageOptions{}
-	// drawImageOptions.GeoM.Translate(-float64(frameWidth)/2, -float64(frameHeight)/2)
-	// drawImageOptions.GeoM.Translate(screenWidth/2, screenHeight/2)
-	i := (count / 5) % frameNum
-	sx, sy := frameOX+i*frameWidth, frameOY
+	if ebiten.IsKeyPressed(ebiten.KeyUp) {
+		p.VelY = -playerSpeed
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyDown) {
+		p.VelY = playerSpeed
+	}
+	if !ebiten.IsKeyPressed(ebiten.KeyUp) && !ebiten.IsKeyPressed(ebiten.KeyDown) {
+		p.VelY = 0
+	}
+
+	p.PosX += p.VelX
+	p.PosY += p.VelY
+}
+
+func (p *Player) Draw(screen *ebiten.Image) {
+	currentFrame := (clock / playerAnimRate) % frameNum
+
+	sx, sy := frameOX+currentFrame*frameWidth, frameOY
 
 	runDrawImageOptions := &ebiten.DrawImageOptions{}
-	runDrawImageOptions.GeoM.Translate(xPos, 0)
-	if ebiten.IsKeyPressed(ebiten.KeyRight) {
-		xPos++
+	runDrawImageOptions.GeoM.Translate(p.PosX, p.PosY)
+
+	if p.VelX > 0 {
 		screen.DrawImage(runnerImage.SubImage(image.Rect(sx, sy, sx+frameWidth, sy+frameHeight)).(*ebiten.Image), runDrawImageOptions)
 	} else {
 		screen.DrawImage(runnerImage.SubImage(image.Rect(0, 0, frameWidth, frameHeight)).(*ebiten.Image), runDrawImageOptions)
 	}
+}
 
+func update(screen *ebiten.Image) error {
+	clock++
 
+	player.Update(screen)
+
+	draw(screen)
+
+	debug(screen)
+
+	return nil
+}
+
+func draw(screen *ebiten.Image) error {
+	if ebiten.IsDrawingSkipped() {
+		return nil
+	}
+
+	player.Draw(screen)
+
+	return nil
+}
+
+func debug(screen *ebiten.Image) {
 	pressed := []ebiten.Key{}
 	for k := ebiten.Key(0); k <= ebiten.KeyMax; k++ {
 		if ebiten.IsKeyPressed(k) {
@@ -66,19 +122,9 @@ func update(screen *ebiten.Image) error {
 		keyStrs = append(keyStrs, p.String())
 	}
 	ebitenutil.DebugPrint(screen, strings.Join(keyStrs, ", "))
-
-
-
-	return nil
 }
 
 func main() {
-	img, _, err := image.Decode(bytes.NewReader(images.Runner_png))
-	if err != nil {
-		log.Fatal(err)
-	}
-	runnerImage, _ = ebiten.NewImageFromImage(img, ebiten.FilterDefault)
-
 	if err := ebiten.Run(update, 320, 240, 2, "Hello, World!"); err != nil {
 		log.Fatal(err)
 	}
